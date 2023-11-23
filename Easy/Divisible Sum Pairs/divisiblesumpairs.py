33f11a238da06b154b5b36eb2b69313caf42d21d
Alexandre Knabah <knabah.alexandre@gmail.com>
2023-11-23 18:08:19 -0300

    Fáceis

---
+++ Easy/Divisible Sum Pairs/divisiblesumpairs.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    
+    i = 0
+    j = 1
+    pairs = 0
+    
+    while i < n:
+        
+        while j < n:
+            
+            if (((ar[i] + ar[j]) % k == 0) and i != j):
+                pairs += 1
+            
+            j += 1
+            
+        i += 1
+        j = i
+        
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
